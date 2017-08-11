f88bcafebddeba6a74591312519e887347f93985
hollyfeldl <hollyfeldl@gmail>
2017-08-10 19:38:37 -0700

    Add variables
     Changes to be committed:
    	new file:   variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+# the variables example
+
+def main():
+
+	# sring with double quotes
+	a = "initial"
+	print(a)
+
+	b, c = 1, 2
+	print(b, c)
+
+	d = True
+	print(d)
+
+	# what prints out for a null
+	e = None 
+	print(e)
+
+	# set a string with single quotes
+	f = 'short'
+	print(f)
+
+main()
