e9dc5913a0ced526e4e6a326997d009e52153ccf
hollyfeldl <hollyfeldl@gmail>
2017-08-11 21:24:24 -0700

    Add exercise
     Changes to be committed:
    	new file:   constants.py

---
+++ constants.py
@@ -0,0 +1,20 @@
+# the constant example
+
+import math
+
+s = "constant"
+
+def main():
+    print(s)
+
+    n = 500000000
+
+    d = 3e20 / n
+
+    print('{:6.0e}'.format(d))
+
+    print(d)
+
+    print(math.sin(n))
+
+main()
