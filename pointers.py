152d80ab80097a3bdb3970ae562e05ed1b831e1d
hollyfeldl <hollyfeldl@gmail>
2017-09-07 20:38:30 -0700

    Recursion example and a place holder for pointers.
     Changes to be committed:
    	new file:   pointers.py
    	new file:   recursion.py

---
+++ pointers.py
@@ -0,0 +1,6 @@
+# pointer example
+
+# python doesn't have pointers but they could be simulated
+# https://stackoverflow.com/questions/1145722/simulating-pointers-in-python
+
+# come back to this one
---
+++ recursion.py
@@ -0,0 +1,15 @@
+#recursion example
+
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
+
+def main():
+
+    # compute the factoral of 7
+    print(fact(7))
+
+
+main()
+
