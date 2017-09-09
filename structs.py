dfdbf7894daec861546f51bc84793a2e8a4dab66
hollyfeldl <hollyfeldl@gmail>
2017-09-09 14:46:55 -0700

    Added the struct example using Python classes
     Changes to be committed:
    	new file:   structs.py

---
+++ structs.py
@@ -0,0 +1,35 @@
+# the structs example
+# in Python let us use classes
+
+class person:
+    def __init__(self, name=None, age=0):
+        self.name = name
+        self.age = age
+    def __repr__(self):
+        # make the representation look like Go structs
+        return ('{%s %s}' 
+                % (repr(self.name), repr(self.age)))
+
+def main():
+
+    print(person("Bob", 20))
+
+    print(person(name="Alice", age=30))
+
+    print(person(name="Fred"))
+
+    # skip pointers, show you can switch order
+    print(person(age=40, name="Ann"))
+
+    s = person(name="Sean", age=50)
+    print(s.name)
+
+    # skip the pointer fun
+    print(s.age)
+
+    # change the age directly
+    s.age = 51
+    print(s.age)
+
+main()
+
