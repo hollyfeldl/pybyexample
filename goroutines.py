f9e8485e61d980be5fe23ae1859cd49476e5a61e
Mark Rank <hollyfeldl@gmail.com>
2017-09-30 08:48:30 -0700

    Start of the coroutine example
     Changes to be committed:
    	new file:   goroutines.py

---
+++ goroutines.py
@@ -0,0 +1,37 @@
+# the gorountines example using asyncio tasks
+
+import asyncio
+
+def f(fromVar):
+    """Undecorated version of F"""
+    for i in range(0,3):
+        show("nodeco", fromVar, i)
+
+@asyncio.coroutine
+def fio(idx):
+    """Decorated version of F for asyncio. swap it around so it can be a generator"""
+    show("deco", "coroutine", idx)
+        
+
+def show(callfrom, msg, idx):
+    """Pull out the print from both"""
+    print(callfrom, msg, ":", idx)
+
+
+def main():
+
+    # first the direct calls to function f
+    f("direct")
+
+    loop = asyncio.get_event_loop()
+    to_do = [fio(i) for i in range(0,3)]
+    wait_coro = asyncio.wait(to_do)
+    res, _ = loop.run_until_complete(wait_coro)
+    loop.close()
+
+    print(len(res))
+
+
+if __name__ == "__main__":
+    main()
+
