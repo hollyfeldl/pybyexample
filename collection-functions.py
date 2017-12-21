e93e6c1ee727939ea14365ff630aa15e7606cd04
Mark <hollyfeldl@gmail.com>
2017-12-20 21:35:08 -0800

    Started collection-functions
     Changes to be committed:
    	modified:   collection-functions.py

--- collection-functions.py
+++ collection-functions.py
@@ -1,7 +1,21 @@
 
 
+def Index(vs, t):
+	for i in range(len(vs)):
+		if vs[i] == t:
+			return i
+	return -1
+
+def Include(vs, t):
+	return Index(vs, t) >= 0
+
+
 def main():
 
+	strs = ["peach", "apple", "pear", "plum"]
+
+	print(Index(strs, "pear"))
+	print(Include(strs, "grape"))
 
 if __name__ == "__main__":
     main()    
