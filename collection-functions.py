23882b0758fafb9b3260813c6194127f043b7ef7
Mark <hollyfeldl@gmail.com>
2018-06-07 20:50:41 -0700

    Finished collection-functions
     Changes to be committed:
    	modified:   collection-functions.py

--- collection-functions.py
+++ collection-functions.py
@@ -1,3 +1,5 @@
+# the collection functions example
+# https://gobyexample.com/collection-functions
 
 
 def Index(vs, t):
@@ -10,12 +12,44 @@ def Include(vs, t):
 	return Index(vs, t) >= 0
 
 
+def GoAny(vs, f):
+	for i in vs:
+		if f(i):
+			return True
+	return False
+
+def GoAll(vs, f):
+	for i in vs:
+		if not f(i):
+			return False
+	return True
+
+def Filter(vs, f):
+	vsf = []
+	for i in vs:
+		if f(i):
+			vsf.append(i)
+	return vsf
+
+def GoMap(vs, f):
+	vsm = []
+	for v in vs:
+		vsm.append(f(v))
+	return vsm
+
 def main():
 
 	strs = ["peach", "apple", "pear", "plum"]
+	strs_nil = ["peach", "apple", "", "pear", "plum"]
+
 
 	print(Index(strs, "pear"))
 	print(Include(strs, "grape"))
+
+	print(GoAny(strs, lambda x: x.startswith('p')))
+	print(GoAll(strs, lambda x: x.startswith('p')))
+	print(Filter(strs, lambda x: False if x.find('e') < 0 else True ))
+	print(GoMap(strs, lambda x: x.upper()))
 
 if __name__ == "__main__":
     main()    
